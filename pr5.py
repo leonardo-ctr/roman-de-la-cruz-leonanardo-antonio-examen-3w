38ecc9eb38629d61ef887f98369a5d5de38f92d0
pollocandente <a.23308051281211@cbtis128.edu.mx>
2024-09-27 13:22:04 -0600

    Add files via upload

---
+++ pr5.py
@@ -0,0 +1,31 @@
+print(" ")#define una linea en blanco
+print(" Roman De la Cruz Leonardo Antonio, numero 1211, 3-w")
+print(" ")#define una linea en blanco
+#solicita los valores
+a = float(input("Ingresa el primer valor: "))
+print(" ")# define una linea en blanco
+b = float(input("Ingresa el segundo valor: "))
+print(" ")# define una linea en blanco
+
+
+if a < b:#marca que el menor sea a
+    menor = a
+else:
+    menor = b#si no es marcara que b es menor
+
+# Escribir el valor menor
+print("El menor de los dos valores es:", menor)#imprime el valor menor
+print(" ")# define una linea en blanco
+
+#solicita los valores para realizar la suma
+num1 = float(input("Ingresa el primer número para sumar: "))
+print(" ")# define una linea en blanco
+num2 = float(input("Ingresa el segundo número para sumar: "))
+print(" ")# define una linea en blanco
+
+suma = num1 + num2#operacion realizada
+
+
+print(" ")# define una linea en blanco
+print("La suma de los dos números es:", suma) #print imprime el valor
+print(" ")# define una linea en blanco
